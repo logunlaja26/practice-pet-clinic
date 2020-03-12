1b05c78b48f15a10323445cbcb302c22a25a3950
logunlaja26 <logunlaja26@gmail.com>
2020-03-11 23:17:34 -0500

    Initial Project Add

---
+++ practice-pet-clinic/src/main/java/com/spring/practicepetclinic/PracticePetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.spring.practicepetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticePetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticePetClinicApplication.class, args);
+	}
+
+}
---
+++ practice-pet-clinic/src/test/java/com/spring/practicepetclinic/PracticePetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.practicepetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticePetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

db3fd56d40f35f7db0c637b622a7ef2682f4c08c
logunlaja26 <logunlaja26@gmail.com>
2020-04-24 15:55:46 -0500

    Added model package

---
+++ practice-pet-clinic/src/main/java/com/spring/practicepetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.spring.practicepetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ practice-pet-clinic/src/main/java/com/spring/practicepetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.spring.practicepetclinic.model;
+
+public class Vet extends Person {
+}
